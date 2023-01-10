1138f86807253f5f14937fd72db3da0dfe307696
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2023-01-10 21:10:13 +0400

    Add files via upload

---
+++ js-4/App.js
@@ -0,0 +1,176 @@
+/* 
+
+
+
+let str = "Salam";
+
+console.log(str.slice(0,4));
+
+
+let str2 = "Leman";
+
+console.log(str2.length);
+
+console.log(str2.substr(0,2));
+
+
+let str3 = "Salam Dunya";
+
+ console.log(str3.replace("Dunya","World"));
+
+
+ console.log(str.concat(" ", "Geceniz xeyre"));
+
+ let student = "Amal saLam necesen"
+
+ console.log(student.charAt(0));
+ console.log(student.indexOf("A"));
+console.log(student.toUpperCase().includes("M"));
+    let nemas = "Ulker Salam";
+
+    console.log(nemas.split(",")); */
+
+
+
+    /* Array (Masivlerin) Metodlari */
+
+    /* Array Concat metod */
+
+
+   /*  let arr = ["Ulker"];
+    let arr2 = ["Aysu"];
+    let arr3 = ["Salam"]
+
+     console.log(arr.concat(arr2,arr3)) */
+
+
+     /*  Array every Metodu */
+
+    /*  let arr = [10,20,30,40];
+
+     console.log(arr.every((x)=>x > 15));
+     console.log(arr.some((x)=> x > 35)); */
+
+
+     /* Array toString */
+
+    /*  let arr = ["salam"];
+
+     console.log(arr.toString()); */
+
+
+    /* Array Splice Metodu */
+
+
+  /*   let names = ["Aysu","Elsen","Murad","Elvin"];
+
+    let x = names.splice(0,2,"Ramin");
+    console.log(x);
+    console.log(names); */
+
+
+    /* Array Join Metodu */
+
+   /*  let names = ["Aysu","Elsen","Murad","Elvin"]
+
+    console.log(names.join(" ")); */
+
+
+    /* Array Slice */
+/* 
+    let names = ["Aysu","Elsen","Murad","Elvin"];
+
+
+    console.log(names.slice(0,2));
+     */
+
+
+    /* Array indexOf */
+
+    /* let names = ["Aysu","Elsen","Murad","Elvin"];
+
+
+    console.log(names.indexOf("Murad")); */
+
+
+
+        /* Array forEach */
+
+       /*  let names = ["Aysu","Elsen","Murad","Elvin"]; */
+
+        
+
+
+     /*    let x = names.forEach((item)=>{
+            console.log(item);
+        }) */
+
+
+       /*  for(let i = 0; i < names.length; i++){
+                  console.log(names[i]);
+        } */
+
+
+
+
+        /*  Array Map metodu */
+
+
+       /*  let num = [1,2,3,4,5];
+
+         let allNum = num.map((x)=>x*2);
+
+         console.log(allNum); */
+
+
+
+
+
+
+      /*    function myFunc(){
+            console.log("Hello");
+         }
+
+         myFunc();
+
+
+         AllFunction = ()=>{
+            console.log("Necesen");
+         }
+
+         AllFunction(); */
+
+
+
+         /* Array Filter  metods*/
+
+
+         /* let obj = [
+            {
+                name:"Ramin", surname:"Israfilli"
+            },
+            {
+                name:"Elvin", surname:"Eliyev"
+            },
+            {
+                name:"Raul",surname:"Israfilli"
+            },
+         ];
+
+
+         let fil = obj.filter((item)=>item.name === "Elvin");
+         console.log(fil); */
+
+
+
+
+
+         
+    
+
+
+ 
+
+
+
+
